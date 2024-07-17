039bf0256bef4293f5c8e74f36e1b45e615745f6
Ravindra <sainiravindra369@gmail.com>
2024-07-17 15:25:42 +0530

    indes.js file changes for the server

--- src/index.js
+++ src/index.js
@@ -1,27 +1,23 @@
 //  require('dotenv').config({path: './env'})
+// require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-import { app } from "./app.js";
+import {app} from './app.js'
 dotenv.config({
     path: './.env'
 })
 
 
 
-
-
 connectDB()
-.then(() =>{
-   app.listen(process.env.PORT || 8000 ,()=>{
-      console.log(`Server is running port :${process.env.PORT}`);
-      
-   })
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
 })
-.catch((error) =>{
-   console.log("MONGO DB connection failed !!!", error);
-
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
 })
-
 
 
 
