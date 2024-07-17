3b557e694b58bc2acec74a5e458bf17355a67241
Ravindra <sainiravindra369@gmail.com>
2024-07-17 15:27:53 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 //  require('dotenv').config({path: './env'})
-// require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 import {app} from './app.js'
